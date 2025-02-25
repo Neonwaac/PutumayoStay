0a9e2cb30d189b60c54fe884d32bcd4645fc02d7
NEONWAAC <jomaparo3103@gmail.com>
2025-02-24 19:59:37 -0500

    RoomsPage Starting search menu layout

---
+++ backend/models/habitacion.js
@@ -0,0 +1,8 @@
+const db = require('../db/db');
+const bcrypt = require('bcrypt');	
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
--- frontend/src/pages/RoomsPage/RoomsPage.jsx
+++ frontend/src/pages/RoomsPage/RoomsPage.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import "./RoomsPage.css";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import RoomsLayout from "../../layouts/RoomsLayout/RoomsLayout";
-function RoomsPage(){
+import { FaSearch } from "react-icons/fa"
+function RoomsPage() {
 
-    return(
+    return (
         <section className="rooms-page">
             <NavigationBar />
-            <h1>RoomsPage</h1>
-            <RoomsLayout/>
+            <h1 className="rooms-page-title">Todas nuestras habitaciones</h1>
+            <div className="rooms-page-filter-container">
+                <div className="rooms-page-filter-search">
+                    <input className="rooms-page-filter-search-input" type="text" placeholder="Realiza tus busquedas aquí"></input>
+                    <button className="rooms-page-filter-search-button"><FaSearch /></button>
+                </div>
+                <div className="rooms-page-filter-category">
+                    <p className="rooms-page-filter-category-title">Selecciona categoría</p>
+                    <button className="rooms-page-filter-category-option">Normal</button>
+                    <button className="rooms-page-filter-category-option">Ejectutivo</button>
+                    <button className="rooms-page-filter-category-option">Familiar</button>
+                    <button className="rooms-page-filter-category-option">De lujo</button>
+                </div>
+            </div>
+            <RoomsLayout />
         </section>
     );
 }
