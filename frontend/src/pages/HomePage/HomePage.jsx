a943c193f10ed530c39381c76bca9188e20e3b96
Neonwaac <jomaparo3103@gmail.com>
2025-02-26 14:10:04 -0500

    if Admin || Empresa show add button

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -4,19 +4,29 @@ import "./HomePage.css";
 import RoomsLayout from "../../layouts/RoomsLayout/RoomsLayout";
 import AppFooter from "../../components/AppFooter/AppFooter.";
 import ReviewsLayout from "../../layouts/ReviewsLayout/ReviewsLayout";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+    const navigate = useNavigate();
+    const goRoomsPage = (e) => {
+        e.preventDefault();
+        navigate("/rooms")
+    }
+    const goReviewsPage = (e) => {
+        e.preventDefault();
+        navigate("/reviews")
+    }
     return (
         <section clasName="home-page">
             <NavigationBar/>
             <div className="rooms-layout-view-all-container">
                 <h1 className="rooms-layout-title">Habitaciones Disponibles</h1>
-                <button className="rooms-layout-view-all-button">Ver todas las habitaciones</button>
+                <button className="rooms-layout-view-all-button" onClick={goRoomsPage}>Ver todas las habitaciones</button>
             </div>
             <RoomsLayout/>
             <div className="reviews-layout-view-all-container">
                 <h1 className="reviews-layout-title">Reseñas de Usuarios</h1>
-                <button className="reviews-layout-view-all-button">Ver más reseñas de usuarios</button>
+                <button className="reviews-layout-view-all-button" onClick={goReviewsPage}>Ver más reseñas de usuarios</button>
             </div>
             <ReviewsLayout/>
             <AppFooter />
--- frontend/src/pages/RoomsPage/RoomsPage.jsx
+++ frontend/src/pages/RoomsPage/RoomsPage.jsx
@@ -2,45 +2,71 @@ import React, { useEffect, useState } from "react";
 import "./RoomsPage.css";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import RoomsLayout from "../../layouts/RoomsLayout/RoomsLayout";
-import { FaSearch, FaPlus } from "react-icons/fa"
+import { FaSearch, FaPlus } from "react-icons/fa";
 import AppFooter from "../../components/AppFooter/AppFooter.";
 import { useNavigate } from "react-router-dom";
 function RoomsPage() {
-    const [user, setUser] = useState("")
-    const navigate = useNavigate();
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        const storedUser = JSON.parse(localStorage.getItem('user'));
-        if(storedUser){
-            setUser(storedUser);
-        }else{
-            navigate("/login")
-        }
-    },[navigate]);
-    return (
-        <section className="rooms-page">
-            <NavigationBar />
-            <h1 className="rooms-page-title">Todas nuestras habitaciones</h1>
-            <div className="rooms-page-filter-container">
-                <div className="rooms-page-filter-search">
-                    <input className="rooms-page-filter-search-input" type="text" placeholder="Busca por nombre de habitación"></input>
-                    <button className="rooms-page-filter-search-button"><FaSearch /></button>
-                </div>
-                <div className="rooms-page-filter-category">
-                    <p className="rooms-page-filter-category-title">¿Que tipo de habitación buscas?</p>
-                    <button className="rooms-page-filter-category-option">Estándar</button>
-                    <button className="rooms-page-filter-category-option">Doble</button>
-                    <button className="rooms-page-filter-category-option">Suite</button>
-                    <button className="rooms-page-filter-category-option">Suite Jr</button>
-                    <button className="rooms-page-filter-category-option">Familiar</button>
-                    <button className="rooms-page-filter-category-option">Penthouse</button>
-                </div>
-            </div>
-            <RoomsLayout />
-            {user && Number(user.rol) === 2 && <button className="rooms-page-add-button"><FaPlus /></button>}
-            <AppFooter />
-        </section>
-    );
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser) {
+      setUser(storedUser);
+    } else {
+      navigate("/login");
+    }
+  }, [navigate]);
+
+  return (
+    <section className="rooms-page">
+      <NavigationBar />
+      <h1 className="rooms-page-title">Todas nuestras habitaciones</h1>
+      <div className="rooms-page-filter-container">
+        <div className="rooms-page-filter-search">
+          <input
+            className="rooms-page-filter-search-input"
+            type="text"
+            placeholder="Busca por nombre de habitación"
+          ></input>
+          <button className="rooms-page-filter-search-button">
+            <FaSearch />
+          </button>
+        </div>
+        <div className="rooms-page-filter-category">
+          <p className="rooms-page-filter-category-title">
+            ¿Que tipo de habitación buscas?
+          </p>
+          <button className="rooms-page-filter-category-option">
+            Estándar
+          </button>
+          <button className="rooms-page-filter-category-option">Doble</button>
+          <button className="rooms-page-filter-category-option">Suite</button>
+          <button className="rooms-page-filter-category-option">
+            Suite Jr
+          </button>
+          <button className="rooms-page-filter-category-option">
+            Familiar
+          </button>
+          <button className="rooms-page-filter-category-option">
+            Penthouse
+          </button>
+        </div>
+      </div>
+      <div className="rooms-page-add">
+        {user && (Number(user.rol) === 2 || Number(user.rol) === 3) && (
+          <h3 className="rooms-page-add-title">🛠️ Agrega una habitación</h3>
+        )}
+        {user && (Number(user.rol) === 2 || Number(user.rol) === 3) && (
+          <button className="rooms-page-add-button">
+            <FaPlus />
+          </button>
+        )}
+      </div>
+      <RoomsLayout />
+      <AppFooter />
+    </section>
+  );
 }
 
 export default RoomsPage;
