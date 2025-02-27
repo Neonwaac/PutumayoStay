8f7dcb8945f7e29bedc5354b0027fcf16898835b
Neonwaac <jomaparo3103@gmail.com>
2025-02-27 00:44:21 -0500

    AddRoomModal , RoomCard(deleteButton) finalized and WORKING

--- frontend/src/components/AddRoomModal/AddRoomModal.jsx
+++ frontend/src/components/AddRoomModal/AddRoomModal.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./AddRoomModal.css";
-import { MdDisabledByDefault } from "react-icons/md";
+import PreviewPhoto from "../../assets/default-preview-photo.png"
 import axios from "axios";
-
+import Swal from "sweetalert2";
 const AddRoomModal = ({ isOpen, onClose }) => {
   const [formData, setFormData] = useState({
     nombre: "",
@@ -17,9 +17,13 @@ const AddRoomModal = ({ isOpen, onClose }) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
-
+  const [previewImage, setPreviewImage] = useState(null);
   const handleFileChange = (event) => {
-    setFormData({ ...formData, foto: event.target.files[0] });
+    const file = event.target.files[0];
+    if (file) {
+      setFormData({ ...formData, foto: file });
+      setPreviewImage(URL.createObjectURL(file));
+    }
   };
 
   const handleSubmit = async (event) => {
@@ -38,96 +42,120 @@ const AddRoomModal = ({ isOpen, onClose }) => {
           "Content-Type": "multipart/form-data",
         },
       });
-      alert("Habitación agregada con éxito");
+      Swal.fire({
+        title: "Habitación publicada correctamente",
+        icon: "success",
+        draggable: true
+    });
+    window.location.reload(true);
       onClose();
     } catch (error) {
       console.error("Error al agregar la habitación", error);
-      alert("Hubo un error al agregar la habitación");
+       Swal.fire({
+        icon: "error",
+        title: "Error al publicar la habitación",
+        text: "Verifica el servidor o la BD",
+    });
     }
   };
-
   return (
     isOpen && (
       <div className="add-room-modal" onClick={onClose}>
-        <div className="add-room-modal-content" onClick={(e) => e.stopPropagation()}>
+        <div
+          className="add-room-modal-content"
+          onClick={(e) => e.stopPropagation()}
+        >
           <h2 className="add-room-modal-title">Agregar Habitación</h2>
           <form className="add-room-modal-form" onSubmit={handleSubmit}>
-            <input
-              id="file-upload"
-              className="add-room-modal-input-image"
-              required
-              type="file"
-              name="foto"
-              onChange={handleFileChange}
-            />
-            <label htmlFor="file-upload" className="add-room-modal-label-image">
-              Seleccionar Imagen
-            </label>
-
-            <label className="add-room-modal-label">Nombre de la habitación:</label>
-            <input
-              className="add-room-modal-input"
-              type="text"
-              name="nombre"
-              placeholder="Dale un nombre a la habitación"
-              value={formData.nombre}
-              onChange={handleChange}
-              required
-            />
-
-            <label className="add-room-modal-label">Descripción:</label>
-            <textarea
-              className="add-room-modal-text-area"
-              name="descripcion"
-              placeholder="Escribe una descripción"
-              value={formData.descripcion}
-              onChange={handleChange}
-              required
-            ></textarea>
-
-            <label className="add-room-modal-label">Capacidad:</label>
-            <input
-              className="add-room-modal-input"
-              type="number"
-              name="capacidad"
-              placeholder="Número de personas máximo Ej. 5"
-              value={formData.capacidad}
-              onChange={handleChange}
-              required
-            />
-
-            <label className="add-room-modal-label">Precio por noche:</label>
-            <input
-              className="add-room-modal-input"
-              type="number"
-              name="precio"
-              placeholder="Precio por noche Ej. 150.000COP"
-              value={formData.precio}
-              onChange={handleChange}
-              required
-            />
-
-            <label className="add-room-modal-label">Categoría:</label>
-            <select
-              className="add-room-modal-select"
-              name="categoria"
-              value={formData.categoria}
-              onChange={handleChange}
-            >
-              <option value={1}>Estándar</option>
-              <option value={2}>Doble</option>
-              <option value={3}>Suite</option>
-              <option value={4}>Suite Jr</option>
-              <option value={5}>Familiar</option>
-              <option value={6}>Penthouse</option>
-            </select>
+            <div className="add-room-modal-left">
+                <img src={previewImage ? previewImage : PreviewPhoto}className="add-room-modal-preview-image"/>
+              <input
+                id="file-upload"
+                className="add-room-modal-input-image"
+                required
+                type="file"
+                name="foto"
+                onChange={handleFileChange}
+              />
+              <label
+                htmlFor="file-upload"
+                className="add-room-modal-label-image"
+              >
+                Seleccionar Imagen
+              </label>
+            </div>
+            <div className="add-room-modal-right">
+              <label className="add-room-modal-label">
+                Nombre de la habitación:
+              </label>
+              <input
+                className="add-room-modal-input"
+                type="text"
+                name="nombre"
+                placeholder="Dale un nombre a la habitación"
+                value={formData.nombre}
+                onChange={handleChange}
+                required
+              />
+              <div className="add-room-modal-item-container">
+              <label className="add-room-modal-label">Capacidad:</label>
+              <input
+                className="add-room-modal-input"
+                type="number"
+                name="capacidad"
+                placeholder="#personas"
+                value={formData.capacidad}
+                onChange={handleChange}
+                required
+              />
+              </div>
+              <div className="add-room-modal-item-container">
+              <label className="add-room-modal-label">Precio x noche:</label>
+              <input
+                className="add-room-modal-input"
+                type="text"
+                name="precio"
+                placeholder="Ej. 100.000COP"
+                value={formData.precio}
+                onChange={handleChange}
+                required
+              />
+              </div>
+              <label className="add-room-modal-label">Categoría:</label>
+              <select
+                className="add-room-modal-select"
+                name="categoria"
+                value={formData.categoria}
+                onChange={handleChange}
+              >
+                <option value={1}>Estándar</option>
+                <option value={2}>Doble</option>
+                <option value={3}>Suite</option>
+                <option value={4}>Suite Jr</option>
+                <option value={5}>Familiar</option>
+                <option value={6}>Penthouse</option>
+              </select>
+              <label className="add-room-modal-label">Descripción:</label>
+              <textarea
+                className="add-room-modal-text-area"
+                name="descripcion"
+                placeholder="Escribe una descripción"
+                value={formData.descripcion}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
 
             <button className="add-room-modal-button" type="submit">
               Agregar
             </button>
 
-            <button className="add-room-modal-close-button" type="button" onClick={onClose}>
-              <MdDisabledByDefault className="add-room-modal-icon" />
+            <button
+              className="add-room-modal-button-close"
+              type="button"
+              onClick={onClose}
+            >
+              Cancelar
             </button>
           </form>
         </div>
--- frontend/src/components/RoomCard/RoomCard.jsx
+++ frontend/src/components/RoomCard/RoomCard.jsx
@@ -1,15 +1,59 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './RoomCard.css';
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
+import Swal from "sweetalert2";
 function RoomCard({key, id, nombre, descripcion, capacidad, foto, precio, categoria}){
+    const [user, setUser] = useState(null);
     const navigate = useNavigate()
     const specificRoom = (e) => {
         e.preventDefault();
         navigate("/rooms/"+id)
     }
+    const deleteRoom = (e) => {
+         e.preventDefault();
+         Swal.fire({
+            title: "Estas seguro de eliminar esta habitación?",
+            text: "No podrás revertir este cambio!",
+            icon: "warning",
+            showCancelButton: true,
+            cancelButtonText: "Cancelar",
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si, eliminar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+              try{
+                axios.delete("http://localhost:8077/rooms/"+id);
+                window.location.reload(true);
+              }catch(error){
+                Swal.fire({
+                        icon: "error",
+                        title: "Error al publicar la habitación",
+                        text: "Verifica el servidor o la BD",
+                    });
+              }
+            }
+          });
+    }
+      useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        if (storedUser) {
+          setUser(storedUser);
+        } else {
+          navigate("/login");
+        }
+      }, [navigate]);
+    
     return(
         <section className="room-card">
             <img className="room-card-image" src={foto} alt="foto"/>
+            {user && Number(user.rol) === 2 && <button className="room-card-delete-button" onClick={deleteRoom}>Eliminar</button>}
             <div className="room-card-content">
                 <h3 className="room-card-title">{nombre}</h3>
                 <p className="room-card-description">{descripcion}</p>
